594ef8a69d9b36ec081cf3697d459196a64754e6
Tarandeep Singh <65854945+Tarandeep-s1ngh@users.noreply.github.com>
2022-04-06 13:44:27 +0530

    minor fix and refactor

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { sageLogo } from "../../assets";
 import { useAuth } from "../../context";
 import "./navbar.css";
 
 export const Navbar = () => {
   const { isLogedIn, logout } = useAuth();
+  const { pathname } = useLocation();
+
+  const isHome = () => (pathname === "/" ? true : false);
 
   return (
-    <nav className="nav-bar">
+    <nav className={isHome() ? "nav-bar" : "nav-bar nav-fixed"}>
       <div className="h3 nav-txt">
         <Link to="/" className="flex-row align-items-center gap0p5">
           <img src={sageLogo} className="img-responsive" alt="Main Logo" />{" "}
@@ -18,10 +21,13 @@ export const Navbar = () => {
         <input type="text" className="search-input" />
         <i className="fas fa-search"></i>
       </div>
-      <Link to="/explore" className="icon-in-nav tooltip">
-        <i className="fas fa-compass"></i>
-        <span className="tooltiptext">Explore</span>
-      </Link>
+
+      {pathname !== "/explore" ? (
+        <Link to="/explore" className="icon-in-nav tooltip">
+          <i className="fas fa-compass"></i>
+          <span className="tooltiptext">Explore</span>
+        </Link>
+      ) : null}
 
       {isLogedIn() ? (
         <>
@@ -35,7 +41,7 @@ export const Navbar = () => {
             <span className="tooltiptext">Liked</span>
           </Link>
           <button onClick={() => logout()} className="icon-in-nav tooltip">
-            <i class="fas fa-sign-out-alt"></i>
+            <i className="fas fa-sign-out-alt"></i>
 
             <span className="tooltiptext">Logout</span>
           </button>
